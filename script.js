38d066f15644dfebf7ccefdca0b865720eed409f
Stefan Tampu <stefan_tampu@yahoo.com>
2023-11-29 19:10:25 +0200

    Adapt index to max-width 768px and 425px screen sizes Create hamburger menu for these sizes and below Also, move logo and hamburger out of nav Turn header into container Move hero out of header

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector("nav");
+const navLinks = document.querySelectorAll(".nav-link");
 const serviceNames = document.getElementsByClassName("service-name");
 const serviceDescriptions = document.getElementsByClassName("service-description");
 const pluses = document.getElementsByClassName("plus");
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    nav.classList.toggle("active");
+}
+
+hamburger.addEventListener("click", mobileMenu);
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    nav.classList.remove("active");
+}
+
+navLinks.forEach(navLink => {
+    navLink.addEventListener("click", closeMenu);
+})
 
 if (serviceNames) {
     for (const serviceName of serviceNames) {
